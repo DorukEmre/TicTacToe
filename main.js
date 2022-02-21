8c15733b1d7f3f47b6ede8ea003cab07a39fbe26
Doruk Emre <devdoruk@outlook.com>
2022-02-21 16:33:17 +0000

    Put functions inside IIFE

--- main.js
+++ main.js
@@ -1,27 +1,136 @@
-const Gameboard = (() => {
+const Game = (() => {
     let availableBoxes = [];
-    let playerBoxes = [];
-    let computerBoxes = [];
     let waitingForReset = 0;
-    let playerScore = 0;
-    let computerScore = 0;
 
-    const drawBoard = (() => {
-        const board = document.querySelector(".board");
-        
-        for (let i = 1; i <= 9; i++) {
-            const box = document.createElement("section");
-            box.className = `box${i}`;
-            board.appendChild(box);
-            availableBoxes.push(`box${i}`)   
+    const createPlayer = (name, score, boxes) => {
+        return { name, score, boxes };
+    };
+    const human = createPlayer("You", 0, []);
+    const computer = createPlayer("Computer", 0, []);
+
+    const gameboard = (() => {
+
+        const drawBoard = () => {
+            const board = document.querySelector(".board");
+            
+            for (let i = 1; i <= 9; i++) {
+                const box = document.createElement("section");
+                box.className = `box${i}`;
+                board.appendChild(box);
+                availableBoxes.push(`box${i}`) 
+                box.addEventListener("click", () => {actions.playerAction(box)})
+            };               
         };
-        
-        console.log(availableBoxes)     
+
+        const resetBoard = () => {
+            const boxes = document.querySelectorAll("[class^=box]");
+            boxes.forEach((item) => item.remove())
+    
+            const win = document.querySelector(".win")
+            if (win.classList.contains("popup")) {
+                win.classList.remove("popup")
+                const restart = document.querySelector(".restart")
+                restart.classList.remove("popup")
+
+            }
+    
+            human.boxes = [];
+            computer.boxes = [];
+            availableBoxes = [];
+            waitingForReset = 0;
+            
+            drawBoard()    
+        }
+
+        const restart = () => {
+            const restart = document.querySelector(".restart")
+            if (restart.classList.contains("popup")) { return }
+
+            human.score = 0;
+            computer.score = 0;
+            const pPlayerScore = document.querySelector(".playerScore");
+            pPlayerScore.textContent = human.score;
+            const pComputerScore = document.querySelector(".computerScore");
+            pComputerScore.textContent = computer.score;
+            gameboard.resetBoard();
+        }
+
+        return { resetBoard, restart } 
     })();
 
-    const boxes = document.querySelectorAll("[class^=box]");
-    boxes.forEach((box) => {
-        box.addEventListener("click", function(e) {
+    
+    const actions = ((box) => {
+        const checkWin = (who, tickedBoxes) => {
+            console.log({tickedBoxes})
+            if ((tickedBoxes.includes("box1") === true && // 1st line
+                tickedBoxes.includes("box2") === true &&
+                tickedBoxes.includes("box3") === true) || 
+                (tickedBoxes.includes("box4") === true && // 2nd line
+                tickedBoxes.includes("box5") === true &&
+                tickedBoxes.includes("box6") === true) || 
+                (tickedBoxes.includes("box7") === true && // 3rd line
+                tickedBoxes.includes("box8") === true &&
+                tickedBoxes.includes("box9") === true) || 
+                (tickedBoxes.includes("box1") === true && // TL diagonal
+                tickedBoxes.includes("box5") === true &&
+                tickedBoxes.includes("box9") === true) || 
+                (tickedBoxes.includes("box3") === true && // TR diagonal
+                tickedBoxes.includes("box5") === true &&
+                tickedBoxes.includes("box7") === true) ||
+                (tickedBoxes.includes("box1") === true && // 1st column
+                tickedBoxes.includes("box4") === true &&
+                tickedBoxes.includes("box7") === true) || 
+                (tickedBoxes.includes("box2") === true && // 2nd column
+                tickedBoxes.includes("box5") === true &&
+                tickedBoxes.includes("box8") === true) || 
+                (tickedBoxes.includes("box3") === true && // 3rd column
+                tickedBoxes.includes("box6") === true &&
+                tickedBoxes.includes("box9") === true)) {
+    
+                const win = document.querySelector(".win")
+                win.classList.add("popup")
+                const restart = document.querySelector(".restart")
+                restart.classList.add("popup")
+    
+                const winMessage = document.querySelector(".winMessage")
+                
+                if (who === "You") { 
+                    human.score++;
+                    const pPlayerScore = document.querySelector(".playerScore");
+                    pPlayerScore.textContent = human.score;
+                    winMessage.textContent = `${who} win this round!`
+                    win.style.borderColor = "#B176AC"
+    
+                } else {
+                    computer.score++;
+                    const pComputerScore = document.querySelector(".computerScore");
+                    pComputerScore.textContent = computer.score;
+                    winMessage.textContent = `${who} wins this round!`
+                    win.style.borderColor = "#75B09A"
+                }
+    
+                waitingForReset = 1;
+                return
+            }   
+            checkDraw();
+        };
+
+        const checkDraw = () => {
+            if (waitingForReset === 0 && availableBoxes.length === 0) { 
+                waitingForReset = 1;
+
+                const win = document.querySelector(".win")
+                win.classList.add("popup")
+                const restart = document.querySelector(".restart")
+                restart.classList.add("popup")
+    
+                const winMessage = document.querySelector(".winMessage")
+                winMessage.textContent = `It's a draw!`
+                win.style.borderColor = "#fff"                
+            }
+        };
+
+        const playerAction = (box) => {
             console.log(`You clicked ${box.className}`);
             if (waitingForReset === 1) { return }
             if (availableBoxes.includes(box.className) === false) { return }
@@ -30,114 +139,51 @@ const Gameboard = (() => {
             cross.className = "cross";
             box.appendChild(cross);
             
-            playerBoxes.push(box.className);
+            human.boxes.push(box.className);
 
             clickedBoxIndex = availableBoxes.indexOf(box.className);
             availableBoxes.splice(clickedBoxIndex, 1);
 
-            checkWin("You", playerBoxes)
-            if (waitingForReset === 0) { computerPlay() }
-        })
-    });
-    const computerPlay = () => {        
-        let computerBoxPick = availableBoxes[Math.floor(Math.random() * availableBoxes.length)];
-        console.log(`Computer picked ${computerBoxPick}`)
+            checkWin("You", human.boxes)
+            if (waitingForReset === 0) { computerAction() }
+        };
+            
+        const computerAction = () => {        
+            let computerBoxPick = availableBoxes[Math.floor(Math.random() * availableBoxes.length)];
+            console.log(`Computer picked ${computerBoxPick}`)
 
-        computerBoxes.push(computerBoxPick)  
-        
-        const computerBox = document.querySelector(`.${computerBoxPick}`)
-        console.log(computerBox)
-        const nought = document.createElement("section");
-        nought.className = "nought";
-        computerBox.appendChild(nought);
+            computer.boxes.push(computerBoxPick)  
+            
+            const computerBox = document.querySelector(`.${computerBoxPick}`)
+            const nought = document.createElement("section");
+            nought.className = "nought";
+            computerBox.appendChild(nought);
 
-        computerBoxPickIndex = availableBoxes.indexOf(computerBoxPick);
-        availableBoxes.splice(computerBoxPickIndex, 1);
+            computerBoxPickIndex = availableBoxes.indexOf(computerBoxPick);
+            availableBoxes.splice(computerBoxPickIndex, 1);
 
-        checkWin("Computer", computerBoxes);
-    }
+            checkWin("Computer", computer.boxes);
+        };
 
-    const checkWin = (who, tickedBoxes) => {
-        console.log({tickedBoxes})
-        if ((tickedBoxes.includes("box1") === true && // 1st line
-            tickedBoxes.includes("box2") === true &&
-            tickedBoxes.includes("box3") === true) || 
-            (tickedBoxes.includes("box4") === true && // 2nd line
-            tickedBoxes.includes("box5") === true &&
-            tickedBoxes.includes("box6") === true) || 
-            (tickedBoxes.includes("box7") === true && // 3rd line
-            tickedBoxes.includes("box8") === true &&
-            tickedBoxes.includes("box9") === true) || 
-            (tickedBoxes.includes("box1") === true && // TL diagonal
-            tickedBoxes.includes("box5") === true &&
-            tickedBoxes.includes("box9") === true) || 
-            (tickedBoxes.includes("box3") === true && // TR diagonal
-            tickedBoxes.includes("box5") === true &&
-            tickedBoxes.includes("box7") === true) ||
-            (tickedBoxes.includes("box1") === true && // 1st column
-            tickedBoxes.includes("box4") === true &&
-            tickedBoxes.includes("box7") === true) || 
-            (tickedBoxes.includes("box2") === true && // 2nd column
-            tickedBoxes.includes("box5") === true &&
-            tickedBoxes.includes("box8") === true) || 
-            (tickedBoxes.includes("box3") === true && // 3rd column
-            tickedBoxes.includes("box6") === true &&
-            tickedBoxes.includes("box9") === true)) {
+        return { playerAction }
+    })();
 
-            if (who === "You") { 
-                playerScore++;
-                const pPlayerScore = document.querySelector(".playerScore");
-                pPlayerScore.textContent = playerScore;
-
-            } else {
-                computerScore++;
-                const pComputerScore = document.querySelector(".computerScore");
-                pComputerScore.textContent = computerScore;
-            }
-
-            const win = document.querySelector(".win")
-            win.classList.add("popup")
-
-            const winMessage = document.querySelector(".winMessage")
-            winMessage.textContent = `${who} won this round!`
-
-            waitingForReset = 1;
-            return
-        }   
-    }
-    const clearBoard = () => {
-        const boxesWithCross = document.querySelectorAll(".cross");
-        boxesWithCross.forEach((item) => item.remove())
-
-        const boxesWithNought = document.querySelectorAll(".nought");
-        boxesWithNought.forEach((item) => item.remove())
-
-        const win = document.querySelector(".win")
-
-        if (win.classList.contains("popup")) {
-            win.classList.remove("popup")
-        }
-
-        waitingForReset = 0;
-        playerBoxes = [];
-        computerBoxes = [];
-        availableBoxes = []
-        for (let i = 1; i <= 9; i++) {
-            availableBoxes.push(`box${i}`)   
-        };      
-    }
+    gameboard.resetBoard();
 
     const resetButton = document.querySelector(".reset")
-    resetButton.addEventListener("click", clearBoard)
+    resetButton.addEventListener("click", gameboard.resetBoard)
+
+    const restartButton = document.querySelector(".restart")
+    restartButton.addEventListener("click", gameboard.restart)
 
     const playAgainButton = document.querySelector(".playAgain")
-    playAgainButton.addEventListener("click", clearBoard)
+    playAgainButton.addEventListener("click", gameboard.resetBoard)
 
     const okButton = document.querySelector(".ok")
-    okButton.addEventListener("click", function(e) {
+    okButton.addEventListener("click", e => {
         const win = document.querySelector(".win")
         win.classList.remove("popup")
+        const restart = document.querySelector(".restart")
+        restart.classList.remove("popup")
     })
-
-
 })();
